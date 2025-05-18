a3ac9c93038ffd50d9c034967cfe5274e8e445e7
Joby-John <joby432john@gmail.com>
2025-05-18 17:15:20 +0530

    setup and basics

---
+++ basics/Basics.java
@@ -0,0 +1,18 @@
+class Basics
+{
+    public static void main(String[] args)
+    {
+        // data types: 
+        //byte :  -128 to 127(8 bits)
+        //short:
+        //int  : -2^31 to 2^31 - 1 (32  bits)
+        //long : -2^62 to 2^63 -1  (64 bits)
+        //float : 6-7 decimal places (32 bits)
+        //double: 15 decimal places (64 bits)
+        //char : store characters 
+        //boolean : true or false 
+        int num = 1000;
+
+        System.out.println(num);
+    }
+}
